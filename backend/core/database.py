8f615bdd9e4a8c87e97880ebeca5ff3b1cf4277b
BZ <114144177+Omerhrr@users.noreply.github.com>
2025-11-01 19:12:04 +0100

    Add checkfirst option to create_db_and_tables
    Updated create_db_and_tables function to check for existing tables before creation.

--- backend/core/database.py
+++ backend/core/database.py
@@ -7,13 +7,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     print("--- Database_url not found")
     DATABASE_URL = "sqlite:///../database.db"
+
 engine = create_engine(DATABASE_URL, echo=True)
 def create_db_and_tables():
     """
     Initializes the database by creating all tables defined by SQLModel.
     """
     print("Creating database and tables...")
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine, checkfirst=True)
+
     print("Database and tables created successfully.")
 
 def get_session():
@@ -22,4 +24,5 @@ def get_session():
     """
     with Session(engine) as session:
         yield session
+
 
